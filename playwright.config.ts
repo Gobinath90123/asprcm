19acdb1d47b47507054de8e6798834f04c369630
Gobinath <gobinath.ravichandran@twilightitsolutions.com>
2025-05-26 17:45:08 +0530

    feat: Setup Playwright for E2E testing

---
+++ playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : 1,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://35.225.79.245:3000/',
+    trace: 'on-first-retry',
+  },
+
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
